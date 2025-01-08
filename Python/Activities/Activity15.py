da2e6bc616eb1605d0c35fdd12e3ea8144aabfe0
deepthi519 <41464283+deepthi519@users.noreply.github.com>
2025-01-08 22:07:39 +0530

    Add files via upload

---
+++ Python/Activities/Activity15.py
@@ -0,0 +1,4 @@
+try:
+    print("name")
+except:
+    NameError(" name is not defined")
---
+++ Python/Activities/Activity18.py
@@ -0,0 +1,22 @@
+import pandas
+dataframe =pandas.read_csv("sample.csv")
+print("Full Data: ")
+print(dataframe)
+	
+print("===============")
+print("Usernames:")
+print(dataframe["Usernames"])
+
+	
+print("===============")
+print("Username:"+dataframe["Usernames"][1]+" password:" + dataframe["Passwords"][1])
+
+#Sort the Usernames column in ascending order
+print("====================================")
+print("Data sorted in ascending Usernames:")
+print(dataframe.sort_values('Usernames'))
+
+#Sort the Passwords column in descending order
+print("====================================")
+print("Data sorted in descending Passwords:")
+print(dataframe.sort_values('Passwords', ascending=False))
