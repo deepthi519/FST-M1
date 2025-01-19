d1762efb14add4b233408087826de9794f7379d4
deepthi519 <41464283+deepthi519@users.noreply.github.com>
2025-01-19 14:02:41 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,8 @@
+
+public class Activity1 {
+public static void main(String[] args) {
+	System.out.println("Hello World");
+	Car mycar = new Car("Deepthi");
+	mycar.car_Details(5, 200, "DeepBlue", 2024);
+}
+}
