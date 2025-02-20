ea05656df944205b7d1edd507808994b5ab5d83e
deepthi519 <41464283+deepthi519@users.noreply.github.com>
2025-02-20 10:57:36 +0530

    Add files via upload

---
+++ Cucumber/Activities/ActivitiesRunner.java
@@ -0,0 +1,38 @@
+package testRunners;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.SelectClasspathResource;
+
+import io.cucumber.junit.platform.engine.Constants;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("Features")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "stepDefinitions")
+@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@activity3")
+@ConfigurationParameter(
+		key = Constants.PLUGIN_PROPERTY_NAME,
+		value = "pretty, html:Reports/HTML_Report.html, junit:Reports/XML_Report.xml, json:Reports/JSON_Report.json"
+		)
+
+/*
+ * @ConfigurationParameter( key = Constants.FILTER_TAGS_PROPERTY_NAME, value =
+ * "@activity1")
+ */
+
+  @ConfigurationParameter( key = Constants.FILTER_TAGS_PROPERTY_NAME, value =
+  "@activity2" )
+ 
+
+@ConfigurationParameter( key = Constants.FILTER_TAGS_PROPERTY_NAME, value =
+		"@activity3" )
+
+@ConfigurationParameter( key = Constants.FILTER_TAGS_PROPERTY_NAME, value =
+		"@activity4" )
+
+
+@ConfigurationParameter(
+		key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@activity5"
+		)
+public class ActivitiesRunner {}
